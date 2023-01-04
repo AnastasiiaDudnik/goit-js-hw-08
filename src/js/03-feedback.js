7cd2b4201a1d0985e9b1829b05e0db5e75b63426
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-01-04 22:59:40 +0200

    3rd not working task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+console.log(form);
+
+const formData = {};
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onFormInput, 250));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function textareaFill() {
+  const savedInput = localStorage.getItem(STORAGE_KEY);
+  const parsedInput = JSON.parse(savedInput);
+  if (parsedInput) {
+    console.log(savedInput);
+    textarea.value = parsedInput;
+  }
+}
+
+textareaFill();
