0524c7c93d3999d71f590ce62cb1ddb19ef85742
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-01-04 23:14:43 +0200

    2nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', function (currentTime) {
+  const seconds = currentTime.seconds;
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
+  console.log('played the video!');
+});
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
