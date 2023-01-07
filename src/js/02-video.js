b77e68316367b794dd3d4936ad60db031b779fd9
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-01-07 23:24:40 +0200

    corrected mistakes in 2nd and 3rd tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -16,19 +16,6 @@ player.on(
 
 const timeOn = JSON.parse(localStorage.getItem(KEY_LOCAL));
 
-player
-  .setCurrentTime(timeOn)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+if (timeOn) {
+  player.setCurrentTime(timeOn);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,33 +3,35 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 console.dir(form);
 
-const formData = {};
 const STORAGE_KEY = 'feedback-form-state';
 
 form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
 
 function onFormInput(event) {
-  formData[event.target.name] = event.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  let formInput = localStorage.getItem(STORAGE_KEY);
+  formInput = formInput ? JSON.parse(formInput) : {};
+  formInput[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formInput));
 }
 
 function onFormSubmit(event) {
   event.preventDefault();
   event.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
-  console.log(form.input);
-  console.log(form.textarea);
+  console.log(form.elements.message.value);
+  console.log(form.elements.email.value);
 }
 
 function textareaFill() {
   const savedInput = localStorage.getItem(STORAGE_KEY);
-  const parsedInput = JSON.parse(savedInput);
-  if (parsedInput) {
-    console.log(parsedInput);
 
-    form.elements.message.value = parsedInput.message;
-    form.elements.email.value = parsedInput.email;
+  if (savedInput) {
+    const parsedInput = JSON.parse(savedInput);
+
+    Object.entries(parsedInput).forEach(([name, value]) => {
+      form.elements[name].value = value;
+    });
   }
 }
 
